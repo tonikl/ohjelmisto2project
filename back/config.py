7b5d362339e10b4deafcb90b1a0957ac133f001e
tonikl <111732474+tonikl@users.noreply.github.com>
2022-12-15 13:45:59 +0200

    Update config.py
    muutettu budget, co2 per km, per flight ja max_distance

--- back/config.py
+++ back/config.py
@@ -1,12 +1,12 @@
 # Game parameters
-max_distance = 1000
+max_distance = 3000
 max_lat_dist = max_distance/50
 max_lon_dist = max_distance/50
 
 co2_initial = 0
-co2_budget = 7000
-co2_per_flight = 50
-co2_per_km = 1
+co2_budget = 200000
+co2_per_flight = 750
+co2_per_km = 15
 
 
 default_starting_point = "EFKE"
